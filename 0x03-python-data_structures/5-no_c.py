80ebdac9f02a3ff8ddfd41679cf44d4946f290fc
Ayienda Brian <ayiendabrian@gmail.com>
2022-09-27 00:58:45 +0200

    buusyness

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = my_string.translate(None, 'cC')
+    newstring = my_string.translate({ord('c'): None})
+    newstring = newstring.translate({ord('C'): None})
+    return (newstring)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return
+
+    maxint = my_list[0]
+    for i in range(1, len(my_list)):
+        if maxint < my_list[i]:
+            maxint = my_list[i]
+        else:
+            continue
+    return (maxint)
