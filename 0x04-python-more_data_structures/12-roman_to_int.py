0c3bbef2d2e49b04f68781dbb4a874019ec0ade8
Ayienda Brian <ayiendabrian@gmail.com>
2022-09-29 03:18:18 +0200

    Mandatory

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    roman_dictnry = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    desm = [roman_dictnry[x] for x in roman_string]
+    introm = 0
+    for i in range(len(desm)):
+        introm += desm[i]
+        if desm[i - 1] < desm[i] and i != 0:
+            introm -= (desm[i - 1] + desm[i - 1])
+    return introm
