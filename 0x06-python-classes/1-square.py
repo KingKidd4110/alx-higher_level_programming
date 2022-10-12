5b0762bd89557d882768b2bb03b4ce35c6178ee1
Ayienda Brian <ayiendabrian@gmail.com>
2022-10-13 00:54:06 +0200

    fst commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square based on first task"""
+
+
+class Square:
+    """perivate instance
+    Attributes:
+        _size(int): side measure
+        """
+
+    def __init__(self, size):
+        """Args:
+        size(int): side measure1."""
+        self._size = size
