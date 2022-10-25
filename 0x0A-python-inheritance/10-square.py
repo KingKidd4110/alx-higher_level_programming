742ed0ec40f9d99d6f525e48ebc036f2c9e81eae
Ayienda Brian <ayiendabrian@gmail.com>
2022-10-26 01:43:11 +0200

    Charm

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+import and rep class Square
+"""
+
+
+class Square:
+    def __init__(self, size):
---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a read function"""
+
+
+def read_file(filename=""):
+    """Reads and prints to stdout"""
+    with open("filename", "r", encoding='utf-8') as fl:
+        print(fl.read(), end="")
